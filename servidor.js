f2bad2e657f21fddf0d5adf887c2d91e189e9f90
Fabián Gálvez <fabian_galvez@hotmail.com>
2015-09-06 15:50:21 -0500

    Configuración Mongoose

--- servidor.js
+++ servidor.js
@@ -1,15 +1,18 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express = require('express');
+var mongoose = require('./config/mongoose');
+//var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public')); 
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile('index.html');
 });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Primera aplicación en http://%s:%s', host, port);
+  console.log('Corriendo en  http://%s:%s', host, port);
 });
